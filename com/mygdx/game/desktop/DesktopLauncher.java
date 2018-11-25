2a6400fcc9aadef3576b42515cf528b22ccebe2e
dzeus14omega <43383923+dzeus14omega@users.noreply.github.com>
2018-11-25 20:57:12 +0700

    on desktop

---
+++ com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.desktop.views.Box2DTutorial;
+
+import TileMapGame.TileMapGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "map1";
+		cfg.useGL30 = true;
+		cfg.width = 960;
+		cfg.height = 960;
+		
+		new LwjglApplication(new Box2DTutorial(), cfg);
+	}
+}
